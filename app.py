58292c1e7d315429324d670b489c7a957ccb2c3e
Ryan <ryan@ispeakcomputer.com>
2025-04-12 16:43:25 -0600

    refactor

--- app.py
+++ app.py
@@ -3,18 +3,14 @@ from openai import OpenAI
 import os
 import string 
 import sys
+# Load environment variables from .env file
+load_dotenv()
+YOUR_API_KEY = os.environ["API_KEY"]
 
 if sys.argv[1] == None:
     print("You must ask perplexity a question. Run again")
 elif sys.argv:
     your_question = sys.argv[1]
-
-# Load environment variables from .env file
-load_dotenv()
-#
-YOUR_API_KEY = os.environ["API_KEY"]
-
-#your_question = input("What would you like to ask? ")
 
 messages = [
     {
